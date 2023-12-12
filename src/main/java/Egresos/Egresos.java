f6948833bb269e5c1576cda69f2ccf15bd36ca9e
Nelsonveliz27 <velizn736@gmail.com>
2023-12-12 01:12:58 -0300

    segundo commit

--- src/main/java/Egresos/Egresos.java
+++ src/main/java/Egresos/Egresos.java
@@ -12,4 +12,34 @@ public class Egresos {
     private JFormattedTextField txtMonto;
     private JFormattedTextField txtGlosa1;
     private JFormattedTextField txtGlosa2;
+    private JFormattedTextField txtNroDocumento;
+    private JFormattedTextField txtNroDocumento11;
+    private JFormattedTextField txtNroDocumento4;
+    private JFormattedTextField txtNroDocumento14;
+    private JFormattedTextField txtNroDocumento2;
+    private JFormattedTextField txtNroDocumento3;
+    private JFormattedTextField txtNroDocumento12;
+    private JFormattedTextField txtNroDocumento13;
+    private JFormattedTextField txtFechaDocumento;
+    private JFormattedTextField txtFechaDocumento2;
+    private JFormattedTextField txtFechaDocumento3;
+    private JFormattedTextField txtFechaDocumento4;
+    private JFormattedTextField txtFechaDocumento11;
+    private JFormattedTextField txtFechaDocumento12;
+    private JFormattedTextField txtFechaDocumento13;
+    private JFormattedTextField txtFechaDocumento14;
+    private JFormattedTextField txtValorDocumento;
+    private JFormattedTextField txtValorDocumento2;
+    private JFormattedTextField txtValorDocumento3;
+    private JFormattedTextField txtValorDocumento4;
+    private JFormattedTextField txtValorDocumento11;
+    private JFormattedTextField txtValorDocumento12;
+    private JFormattedTextField txtValorDocumento13;
+    private JFormattedTextField txtValorDocumento14;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+
+    }
 }
